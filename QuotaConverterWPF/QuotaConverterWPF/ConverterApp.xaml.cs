880723244f17c9faf9832446fb380c6a70afd0d0
Artur Perwenis <aperwenis@gmail.com>
2019-11-12 22:55:10 +0100

    Initial commit from SVN to git.

---
+++ QuotaConverterWPF/QuotaConverterWPF/ConverterApp.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace QuotaConverterWPF
+{
+    /// <summary>
+    /// Interaction logic for ConverterApp.xaml
+    /// </summary>
+    public partial class ConverterApp : Application
+    {
+    }
+}
---
+++ QuotaConverterWPF/QuotaConverterWPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using QBits.Intuition.Text;
+using System.Reflection;
+using QBits.Intuition.Assemblies;
+
+namespace QuotaConverterWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = string.Format("Quota converter v{0}", AssemblyHelper.AssemblyVersion(WhichAssembly.ThisAssembly));
+        }
+
+        private void NumQuotaString_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double inValue;
+            if (double.TryParse(NumQuotaString.Text, out inValue))
+            {
+                string quotaString = FinancialHelper.ConvertQuotaToText(inValue);
+                QuotaText.Text = quotaString;
+            }
+        }
+    }
+}
